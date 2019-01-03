86ad8ccd5a80dc2ef5a2f5f3c93f960ed9cc9058
mdcanty <45926696+mdcanty@users.noreply.github.com>
2019-01-03 12:13:28 -0500

    Add files via upload
    Added that big program we did. Change the variables so it's not as obvious.

---
+++ highLow.java
@@ -0,0 +1,72 @@
+import acm.program.*;
+import java.text.NumberFormat;
+import acm.util.*;
+
+public class highLow extends ConsoleProgram {
+        
+  
+  public void run () { 
+    int points = 1000; int risk = 0; int prediction = 0; int guess = 0;
+      
+      println("High Low Game");
+
+      println("RULES");
+      println("Numbers 1 through 6 are low");
+      println("Number 7 is neither high nor low");
+      println("Numbers 8 through 13 are high");
+
+        do {
+          guess++;
+          println("You have " + points + ".");
+          if (points <= 0) {
+            println("Ran out of cash.");
+            println("Guesses: " + guess);
+            break;
+          }
+          
+          else {
+        
+            prediction = readInt("Enter prediction: ");
+            risk = readInt("Enter amount of cash you want to risk: ");
+            points = play(points, risk, prediction);
+          }
+         
+        } while(true);
+  }
+
+
+
+public int play(int points, int risk, int prediction) {
+        
+        RandomGenerator rg = new RandomGenerator();
+        int num = rg.nextInt(1, 13);
+
+        println("Number is " + num);
+
+        if ((num <= 6) && (prediction == 0)) {
+                println("You win " + risk + " points");
+                points = points + risk;
+
+                return points;
+        }
+
+        else if ((num >= 8) && (prediction == 1)) {
+                println("You win " + risk + " points");
+                points = points + risk;
+
+                return points;
+        }
+
+        else {
+                println("You lose " + risk + " points");
+                points = points - risk;
+                return points;
+        }
+
+
+}
+}
+
+
+
+
