e1a68275928a0fd01d8762fef83a8f75700ccf06
mdcanty <mcanty@tutanota.com>
2019-07-12 14:31:12 -0700

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -3,13 +3,11 @@ import acm.util.*;
 
 public class GuessingGame extends ConsoleProgram { 
     public void run () { 
-
         RandomGenerator rg = new RandomGenerator();
         int x = rg.nextInt(1,2);
         int y = readInt("Enter a number between 1 and 20: ");
         println("Computer's number: " + x);
         println("Player's number: " + y);
-
         if (x = =y) {
             println("You won!");
         }
